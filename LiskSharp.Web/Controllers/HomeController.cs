6cdc685afe198e65694b72cf819e4ea0b6aa8a33
Rajbandi <raj.bandi@hotmail.com>
2016-06-14 19:59:09 +1000

    Renamed to LiskSharp.Web

--- LiskSharp.Web/Controllers/HomeController.cs
+++ LiskSharp.Web/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 
-namespace LiskSharp.DappMan.Web.Controllers
+namespace LiskSharp.Web.Controllers
 {
     public class HomeController : Controller
     {
