91f238b4a6604a55334c2b780e198b6fac78eb31
Rajbandi <raj.bandi@hotmail.com>
2016-06-22 21:40:52 +1000

    fixed unit test

--- LiskSharp.Tests/DappTests.cs
+++ LiskSharp.Tests/DappTests.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
-using System.Net;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-using LiskSharp.Core.Common;
-using LiskSharp.Core.Extensions;
-
-using System.IO.Compression;
 using System.Linq;
 using LiskSharp.DappMan.Common;
 using LiskSharp.DappMan.Helpers;
-using LiskSharp.Tests.Internals;
 using NUnit.Framework;
 
 
@@ -36,17 +26,26 @@ namespace LiskSharp.Tests
         public static async void TestSdkLink()
         {
             var gitUrl = "https://github.com/LiskHQ/lisk-dapps-sdk";
-            var repoName = gitUrl.Split("/".ToCharArray()).Last() + "-development";
+            var repoName = gitUrl.Split("/".ToCharArray()).Last();
+
+            var targetFolder = Path.GetTempPath();
+            Debug.WriteLine(targetFolder);
 
             await DappManHelper.DownloadSdk(new GitOptions
             {
                 FileName = repoName,
                 GitUrl = gitUrl,
                 ReplaceExisting = true,
-                RootFolder = "c:\\Temp"
+                RootFolder = Path.GetTempPath()
             });
-        }
 
+            var repoFile = Path.Combine(targetFolder, repoName + "-development");
+            Debug.WriteLine(repoFile);
+            var isDownloaded = File.Exists(repoFile+".zip");
+            Assert.IsTrue(isDownloaded, "Unable download dapp archive");
+            var isExtracted = Directory.Exists(repoFile);
+            Assert.IsTrue(isExtracted, "Unable to extract dapp archive");
+        }
         
         [Test]
         public async void TestLocalStorage()
